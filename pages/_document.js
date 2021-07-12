492814dc57cf0d42d9db3a178f0a764d3e36d318
Elvis Duru <duruelvis95@gmail.com>
2021-07-12 04:04:57 +0100

    Facebook Pixel

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@
 
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { FB_PIXEL_ID } from '../lib/fpixel'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -52,7 +53,30 @@ export default class MyDocument extends Document {
             href="/favicon-16x16.png"
           />
           <link rel="shortcut icon" href="/favicon.ico" />
-          <!-- Facebook Pixel Code --><script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '345408680446885'); fbq('track', 'PageView');fbq('track', 'Schedule');fbq('track', 'Lead');</script><noscript> <img height="1" width="1" src="https://www.facebook.com/tr?id=345408680446885&ev=PageView&noscript=1"/></noscript><!-- End Facebook Pixel Code -->
+          {/* Global Site Code Pixel - Facebook Pixel */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', ${FB_PIXEL_ID});fbq('track', 'PageView');fbq('track', 'Lead');fbq('track', 'Schedule');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
