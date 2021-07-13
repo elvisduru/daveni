f3ff5c7fb0e710647af895d479a8174525f46514
Elvis Duru <duruelvis95@gmail.com>
2021-07-13 14:37:41 +0100

    Fixed Webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // webpack5: true,
-  future: { webpack5: true },
+  webpack5: true,
   webpack: config => {
     // Unset client-side javascript that only works server-side
     config.resolve.fallback = { fs: false, module: false }
