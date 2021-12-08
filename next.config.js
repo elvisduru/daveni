c8d52d0eab85eb633de287b4d3c40dda58cbdc25
Elvis Duru <duruelvis95@gmail.com>
2021-12-08 21:18:17 +0100

    Update packages

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  webpack5: true,
-  webpack: config => {
-    // Unset client-side javascript that only works server-side
-    config.resolve.fallback = { fs: false, module: false }
-
-    return config
-  },
-}
